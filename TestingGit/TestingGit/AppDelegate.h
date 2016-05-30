7ef71034ec9e3edab6c16f45d91bb534dd417dad
Reeta S Dubey <reetasdubey@reetas-intelimac.local>
2016-05-30 14:38:30 +0530

    #Initial Commit

---
+++ TestingGit/TestingGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingGit
+//
+//  Created by Reeta S Dubey on 30/05/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingGit/TestingGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingGit
+//
+//  Created by Reeta S Dubey on 30/05/16.
+//  Copyright © 2016 Robosoft Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
